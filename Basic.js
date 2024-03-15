3a3c6a052fea038f55d69be608d1677fa8a5cd92
Ayush-Yadav <ayush421301@gmails.com>
2024-03-16 03:02:27 +0530

    4th Module Added

--- Basic.js
+++ Basic.js
@@ -1,7 +1,11 @@
+// Module 1
+
+
+
 // // console.log('Hi')
 // const accountNumber = 98076
 // let name = "Ayush"
-// var accountPass ="GhrcemPune"
+// var accountPass ="Ghrcem"
 // accountCity="Sydney"
 // let accountState= "TA"
 
@@ -15,11 +19,33 @@
 
 
 
+// Module 2
+
 
 
 // str function start here 
 
-let str1 = "Hi____"
-let str2 = "Nodemon"
-str3 = str1+str2
-console.log(str3)
+// let str1 = "Hi____"
+// let str2 = "Nodemon"
+// str3 = str1+str2
+// console.log(str3)
+
+
+
+// Module 3
+
+
+
+// increment operator
+
+// using for operator
+
+// let gameCounter=100
+// for(;gameCounter<151;)
+// {
+//     ++gameCounter;
+// console.log(gameCounter)
+// }
+
+
+// Module 4
