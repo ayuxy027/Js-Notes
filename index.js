ed667cb3b107ae3cea0421bd90ae742a1ce4ec4f
Ayush-Yadav <ayush421301@gmails.com>
2024-03-16 02:54:12 +0530

    Js Notes

---
+++ index.js
@@ -0,0 +1,25 @@
+// // console.log('Hi')
+// const accountNumber = 98076
+// let name = "Ayush"
+// var accountPass ="GhrcemPune"
+// accountCity="Sydney"
+// let accountState= "TA"
+
+// console.table({ 
+//     AccountNumber: accountNumber,
+//     Name: name,
+//     AccountPass: accountPass,
+//     AccountCity: accountCity,
+//     AccountState: accountState
+// })
+
+
+
+
+
+// str function start here 
+
+let str1 = "Hi____"
+let str2 = "Nodemon"
+str3 = str1+str2
+console.log(str3)
