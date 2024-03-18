0c6bd293b1eef9b2b7ad314f8149f01ffd34e211
Ayush-Yadav <ayush421301@gmails.com>
2024-03-19 03:31:30 +0530

    Completed Arrays

--- Notes/04Arrays.js
+++ Notes/04Arrays.js
@@ -1,5 +1,11 @@
-// Array Handbook 
+// Array handbook
 
+// Array:   push(), pop(), shift(), unshift(), splice(), slice(),
+// concat(), forEach(), map(), filter(), reduce(), find(), sort()
+
+// Run each function to see the output, play and learn by doing.
+
+// push()
 
 // push ---> adds a new element to the array from the end of the array
 
@@ -22,3 +28,30 @@
 
 //shift ---> removes the element from the array from the start of the array
 
+// const initialArray = [1, 2, 3, 4, 5];
+// initialArray.shift();
+// console.log(initialArray);
+
+// similarly unshift() adds from start 
+
+// const initialArray = [1, 2, 3, 4, 5];
+// initialArray.unshift("abc");
+// console.log(initialArray);
+
+
+//concat() ---> to merge 2 arays 
+
+
+// const initialArray = [1, 2, 3, 4, 5];
+// const secondArray = [6,7,8,9,10];
+// console.log(initialArray.concat(secondArray));
+
+
+// forEach() ---> to perform on an operation on each element
+
+//  const initialArray = [1, 2, 3, 4, 5];
+
+//  function logThing(str){
+//     console.log(str + ' this is a string');
+//  }
+//  initialArray.forEach(logThing); 
