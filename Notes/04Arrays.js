f305e4e9f55211ccbc836c38fc96f32cfd7dd189
Ayush-Yadav <ayush421301@gmails.com>
2024-03-18 08:53:49 +0530

    Updated Array Module

---
+++ Notes/04Arrays.js
@@ -0,0 +1,24 @@
+// Array Handbook 
+
+
+// push ---> adds a new element to the array from the end of the array
+
+
+// const initialArray = [1, 2, 3, 4, 5];
+// initialArray.push(2);
+// console.log(initialArray);
+
+// // output is [ 1, 2, 3, 4, 5, 2 ]
+
+
+//pop ---> removes the element from the array from the end of the array
+
+
+// const initialArray = [1, 2, 3, 4, 5];
+// initialArray.pop();
+// console.log(initialArray);
+
+// // Output is [ 1, 2, 3, 4]
+
+//shift ---> removes the element from the array from the start of the array
+
