f198fe30d2de38bc16fcfb8cb49eb69a29e73f3c
Ayush-Yadav <ayush421301@gmails.com>
2024-03-18 08:18:44 +0530

    Numbers Module Added

---
+++ Notes/03Numbers.js
@@ -0,0 +1,17 @@
+// Numbers Handbook 
+
+// parseInt --> converts string to number 
+// parseFloat --> converts string to number allowing decimal values
+
+// const a = '42';
+// console.log(a);         
+// console.log(typeof a);   // Output: string
+// const b = '42';
+// console.log(a);          
+// console.log(parseInt(b)); 
+// console.log(typeof b);   // Output: string
+// console.log(typeof parseInt(b)); // Output:Number
+
+
+// console.log(parseInt(3.14)); // Output:3
+
