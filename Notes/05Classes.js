0c94096108b3f2fa67e522a6ac172ab84e666920
Ayush-Yadav <ayush421301@gmails.com>
2024-03-19 04:27:32 +0530

    Classes Module Created

---
+++ Notes/05Classes.js
@@ -0,0 +1,28 @@
+// class can help create a template which can be used to render individual templates data 
+
+
+// class Animals {
+//     constructor(names, legs, speaks) {
+//       this.names = names;
+//       this.legs = legs;
+//       this.speaks = speaks;
+//     }
+    
+//     speak() {
+//       console.log("Hi there " + this.speaks);
+//     }
+//   }
+  
+  // let dog = {
+  //     name : 'Dog',
+  //     legs : 4,
+  //     speaks :'bhow bhow',
+  // }  WRONG WAY xXxXxXxXxXxXxXxXxXxX
+  
+//   let dog = new Animals("dog", 4, "bhow bhow");
+//   let cat = new Animals("cat", 4, "meow meow");
+//   cat.speak();
+
+//   Result : Hi there meow meow (open console)
+  
+// To add more we can add more paramenters in Construct function
