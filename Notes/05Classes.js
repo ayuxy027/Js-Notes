667a8eb895136a00498a7018fcc7cbd1ad2d03b4
Ayush-Yadav <ayush421301@gmails.com>
2024-03-19 05:08:54 +0530

    JSON and Math Modules Added

--- Notes/05Classes.js
+++ Notes/05Classes.js
@@ -1,5 +1,10 @@
 // class can help create a template which can be used to render individual templates data 
 
+// this describes the arguments to the template 
+
+
+// ex-- 
+
 
 // class Animals {
 //     constructor(names, legs, speaks) {
@@ -19,10 +24,24 @@
   //     speaks :'bhow bhow',
   // }  WRONG WAY xXxXxXxXxXxXxXxXxXxX
   
-//   let dog = new Animals("dog", 4, "bhow bhow");
+//   let dog = new Animals("dog", 4, "bhow bhow"); // create object
 //   let cat = new Animals("cat", 4, "meow meow");
-//   cat.speak();
+//   cat.speak(); //  call the function on object
 
 //   Result : Hi there meow meow (open console)
   
+
 // To add more we can add more paramenters in Construct function
+
+
+
+// Date Class 
+
+// const date = new Date();
+// console.log(
+//   "Day: " + date.getDate(),
+//   "Month: " + (date.getMonth() + 1),
+//   "Year: " + date.getFullYear(),
+//   "Hours: " + date.getHours(),
+//   "Minutes: " + date.getMinutes()
+// );
---
+++ Notes/06JSON.js
@@ -0,0 +1,27 @@
+//JSON.stringify
+//JSON.parse
+
+// const Users = '{"name":"Ayush", "email":"ayush@gmail.com", "age":20}';
+
+//we can't call the Users now because it is in strings
+
+//here we use JSON.parse to convert it  into objects format 
+
+// const user = JSON.parse(Users);
+// console.log(user["name"]);
+
+// Output ---> Ayushs
+
+
+// JSON.stringify
+
+// const Users = 
+// {
+//     name : 'ayush',
+//     age : '1234',
+// };    
+
+// const user = JSON.stringify(Users);
+// console.log(user);
+
+// // output --> {"name":"ayush","age":"1234"}
