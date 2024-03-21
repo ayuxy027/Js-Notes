c1c21410c72d9ff60e86713024042c4c3bbfb293
Ayush-Yadav <ayush421301@gmails.com>
2024-03-22 01:15:06 +0530

    DOM Module Created in Folder

---
+++ Notes/10DOM/10DOM.js
@@ -0,0 +1,14 @@
+// DOM Manipulations
+
+// 4 Pillars of DOM
+
+// 1. Element Selection
+
+var a = document.querySelector("h1");
+console.log(a);
+
+// 2. Changing HTML
+
+// 3. Changing CSS
+
+// 4. Event Listeners
